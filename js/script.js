8c71284f4f01d92f0eecd769bc11c9d11a71b149
Thivagar <thivagars@techaffinity.com>
2021-08-20 13:08:37 +0530

    fetch URL to the Iframe source script added

--- js/script.js
+++ js/script.js
@@ -665,3 +665,6 @@ var WebFormEvent = {
     }
 }
 
+var loc = window.location.toString(), params = loc.split('?')[1], iframe = document.getElementById('iframe');
+if (params) { iframe.src = iframe.src + '?' + params; }
+
